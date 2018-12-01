f765da11d5d12821c79e2c74b3c048560dc02056
caz yokoyama <caz@caztech.com>
2018-12-01 09:11:37 -0800

    delete all old photos instead of 15 days ago.

--- clean/cleanup.sh
+++ clean/cleanup.sh
@@ -7,14 +7,21 @@
 
 TOP_DIR=/srv/www-html/html/mobilewebcam
 PHOTO_DIR=archive
-declare -i KEEP=10
-declare -i daysago=15
+KEEP=10 # days
+deletestart=`date --date="${KEEP} days ago" +%Y%m%d`
 
-while [ ${daysago} -gt ${KEEP} ]; do
-    day=`date --date="${daysago} days ago" +%Y-%m-%d`
-    echo delete ${TOP_DIR}/*/${PHOTO_DIR}/${day}
-    sudo -u apache rm -rf ${TOP_DIR}/*/${PHOTO_DIR}/${day}
-    echo $((daysago--)) >/dev/null
+for photo_dir in ${TOP_DIR}/*/${PHOTO_DIR}; do
+    cd ${photo_dir}; ls -1d 20??-??-?? |\
+    while read dir; do
+	year=`echo ${dir} | cut -d"-" -f1`
+	month=`echo ${dir} | cut -d"-" -f2`
+	date=`echo ${dir} | cut -d"-" -f3`
+	dir_date=${year}${month}${date}
+	if [ ${dir_date} -lt ${deletestart} ]; then
+	    echo delete ${photo_dir}/${dir}
+	    sudo -u apache rm -rf ${photo_dir}/${dir}
+	fi
+    done
 done
 
 exit 0
