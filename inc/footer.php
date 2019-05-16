0b55d01cea861c08c13dde3fcd65438812a84b29
caz yokoyama <caz@caztech.com>
2019-05-15 19:27:09 -0700

    robot does not reach me by email.

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,6 @@
 <div id='footer'>
   <?php
     $version = $VERSION;
-    print "<p>mobilewebcam, v$version<a href=\"http://www.caztech.com/glider\"</a>, <a href=\"http://www.caztech.com/glider\">www.caztech.com/glider</a>, <a href=\"mailto:caz@caztech.com\">caz@caztech.com</a></p>";
+    print "<p>mobilewebcam, v$version<a href=\"http://www.caztech.com/glider\"</a>, <a href=\"http://www.caztech.com/glider\">www.caztech.com/glider</a>, <a href=\"mailto:caz at caztech dot com\">caz at caztech dot com</a></p>";
   ?>
 </div>
